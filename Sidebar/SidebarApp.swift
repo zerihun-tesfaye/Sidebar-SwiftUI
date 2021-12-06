414a742b37d693b4aa2f748e81f2c3b82b775716
zerihun-tesfaye <79422161+zerihun-tesfaye@users.noreply.github.com>
2021-12-06 08:49:53 +0300

    Initial Commit

---
+++ Sidebar/SidebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SidebarApp.swift
+//  Sidebar
+//
+//  Created by Zerihun Tesfaye on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SidebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
